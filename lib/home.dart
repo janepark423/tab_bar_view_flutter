c76572d49af383af4cef6bac702f0dc610bd282a
Jain <janepark423@gmail.com>
2022-11-20 13:38:14 +0900

    한글 깨짐 해결

--- lib/home.dart
+++ lib/home.dart
@@ -42,8 +42,10 @@ class _HomeState extends State<Home> {
         notchMargin: 10,
         child:Container(
           height:60,
-          child:
-          Row(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+             Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               MaterialButton(
@@ -92,8 +94,60 @@ class _HomeState extends State<Home> {
               ),
             ],
           ),
-        )
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                MaterialButton(
+                    minWidth: 40,
+                    onPressed: (){
+                      setState(() {
+                        currentScreen = Profile();
+                        currentTab=2;
+                      });
+                    },
+                    child:Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.person,
+                          color: currentTab == 2? Colors.blue: Colors.grey,
+                        ),
+                        Text(
+                          'Profile',
+                          style: TextStyle(color: currentTab == 2? Colors.blue: Colors.grey),
+                        )
+                      ],
+                    )
+                ),
+                MaterialButton(
+                    minWidth: 40,
+                    onPressed: (){
+                      setState(() {
+                        currentScreen = Chat();
+                        currentTab=1;
+                      });
+                    },
+                    child:Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.chat,
+                          color: currentTab == 1? Colors.blue: Colors.grey,
+                        ),
+                        Text(
+                          'Chat',
+                          style: TextStyle(color: currentTab == 1? Colors.blue: Colors.grey),
+                        )
+                      ],
+                    )
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
+      )
     );
   }
 }
