5e5271b00ab76f50456352e15a300cd44cacb694
Jain <janepark423@gmail.com>
2022-11-20 13:32:32 +0900

    first commit

--- lib/home.dart
+++ lib/home.dart
@@ -22,7 +22,7 @@ class _HomeState extends State<Home> {
     Setting(),
     ];
 
-  final pageStorageBucket bucket = PageStorageBucket();
+  final PageStorageBucket bucket = PageStorageBucket();
   Widget currentScreen = Dashboard();
 
   @override
@@ -42,8 +42,9 @@ class _HomeState extends State<Home> {
         notchMargin: 10,
         child:Container(
           height:60,
-          child:Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child:
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               MaterialButton(
                 minWidth: 40,
@@ -72,25 +73,25 @@ class _HomeState extends State<Home> {
                   onPressed: (){
                     setState(() {
                       currentScreen = Chat();
-                      currentTab=0;
+                      currentTab=1;
                     });
                   },
                   child:Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Icon(
-                        Icons.dashboard,
-                        color: currentTab == 0? Colors.blue: Colors.grey,
+                        Icons.chat,
+                        color: currentTab == 1? Colors.blue: Colors.grey,
                       ),
                       Text(
-                        'Dashboard',
-                        style: TextStyle(color: currentTab == 0? Colors.blue: Colors.grey),
+                        'Chat',
+                        style: TextStyle(color: currentTab == 1? Colors.blue: Colors.grey),
                       )
                     ],
                   )
               ),
             ],
-          )
+          ),
         )
       ),
     );
